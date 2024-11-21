6c26c1197b4a3004d528a86d787cbcd2fd21b3dc
baquwerdz <107581575+baquwerdz@users.noreply.github.com>
2024-11-21 19:57:21 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+twodlist = [[10, 20 , 30],[100, 200, 300]]
+print(len(twodlist))
+
+print(twodlist[1][1])
+print(twodlist[0][2])
+
+rows = int(input("How many rows do you want?"))
+columns = int(input("How many columns do you want?"))
+
+matrix
